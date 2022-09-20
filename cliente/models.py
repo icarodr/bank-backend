43cfe0ed5287bfdc31c83dd1bb143fad0ab6e8f8
icarodr <it46009@gmail.com>
2022-09-20 09:08:36 -0300

    first commit

---
+++ cliente/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Usuarios(models.Model):
+    cpf = models.CharField(max_length=11)
+    senha = models.CharField(max_length=255)
+    block = models.BooleanField()
+
+class Cadastro(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    dataNascimento = models.DateField()
+    genero = models.CharField(max_length=255)
+
+class Endereco(models.Model):
+    cep = models.CharField(max_length=8)
